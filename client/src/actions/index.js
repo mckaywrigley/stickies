7be68a4894fee920ec728930690c23dd519f7f24
Mckay <mckaywrigley@gmail.com>
2019-04-27 14:28:59 -0700

    creates getStickies

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,3 +2,15 @@ import axios from "axios";
 import { GET_STICKIES } from "./types";
 
 const url = "https://mckay-stickies.herokuapp.com";
+
+export const getStickies = id => dispatch => {
+  axios
+    .get(`${url}/api/stickies`)
+    .then(res => {
+      dispatch({
+        type: GET_STICKIES,
+        payload: res.data
+      });
+    })
+    .catch(err => console.log(err));
+};
--- client/src/reducers/stickyReducer.js
+++ client/src/reducers/stickyReducer.js
@@ -6,6 +6,11 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case GET_STICKIES:
+      return {
+        ...state,
+        stickies: action.payload
+      };
     default:
       return state;
   }
