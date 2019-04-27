f667e57401f497c8c93c76ecbd47eb006cc04c86
Mckay <mckaywrigley@gmail.com>
2019-04-26 20:34:38 -0700

    creates GET sticky/stickies endpoints

--- routes/stickyRoutes.js
+++ routes/stickyRoutes.js
@@ -11,13 +11,36 @@ const Sticky = require("../models/Sticky");
 
 // ---Endpoints---
 
-// Read
+// Read---
 
-// Create
+// Gets all stickies.
+router.get("/", (req, res) => {
+  Sticky.find()
+    .then(stickies => {
+      return res.status(200).json(stickies);
+    })
+    .catch(err => {
+      return res.status(404).json({ error: "Stickies could not be found." });
+    });
+});
 
-// Delete
+// Gets a single sticky.
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Sticky.findById(id)
+    .then(sticky => {
+      return res.status(200).json(sticky);
+    })
+    .catch(err => {
+      return res.status(404).json({ error: "Sticky could not be found." });
+    });
+});
 
-// Update
+// Create---
+
+// Delete---
+
+// Update---
 
 // Export
 module.exports = router;
