6002f6e4426d13be829a7a9d6e67d78b74d138c6
Mckay <mckaywrigley@gmail.com>
2019-04-27 14:16:58 -0700

    configures redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(null, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
